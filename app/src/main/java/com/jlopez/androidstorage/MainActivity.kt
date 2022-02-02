1e07661fa0a769609891749f9ee624fd50967ba8
Jose Lopez <jlopez@motus.com>
2022-02-02 13:31:47 -0600

    Added saving into external storage

--- app/src/main/java/com/jlopez/androidstorage/MainActivity.kt
+++ app/src/main/java/com/jlopez/androidstorage/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.jlopez.androidstorage
 
+import android.content.ContentValues
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -18,12 +24,21 @@ import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.lang.Exception
 import java.util.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var internalStoragePhotoAdapter: InternalStoragePhotoAdapter
+    private lateinit var externalStoragePhotoAdapter: SharedPhotoAdapter
+
+    private var readPermissionGranted = false
+    private var writePermissionGranted = false
+
+    //Array of strings because want to ask for permissions for every permission not granted
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +55,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        externalStoragePhotoAdapter = SharedPhotoAdapter {
+
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            readPermissionGranted = permissions[android.Manifest.permission.READ_EXTERNAL_STORAGE] ?: readPermissionGranted
+            writePermissionGranted = permissions[android.Manifest.permission.WRITE_EXTERNAL_STORAGE] ?: writePermissionGranted
+        }
+
+        updateOrRequestPermissions()
+
         val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
             val isPrivate = binding.switchPrivate.isChecked
+            val isSavedSuccessfully = when {
+                isPrivate -> savePhotoToInternalStorage(UUID.randomUUID().toString(), it)
+                writePermissionGranted -> savePhotoToExternalStorage(UUID.randomUUID().toString(), it)
+                else -> false
+            }
             if(isPrivate) {
-                val isSavedSuccessfully =  savePhotoToInternalStorage(UUID.randomUUID().toString(), it)
-                if(isSavedSuccessfully) {
-                    loadPhotosFromInternalStorageIntoRecyclerView()
-                    Toast.makeText(this, "Photo saved successfully", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Photo was not able to be saved", Toast.LENGTH_SHORT).show()
-                }
+                loadPhotosFromInternalStorageIntoRecyclerView()
+            }
+            if(isSavedSuccessfully) {
+                Toast.makeText(this, "Photo saved successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Failed to save photo", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -60,6 +90,76 @@ class MainActivity : AppCompatActivity() {
 
         setupInternalStorageRecyclerView()
         loadPhotosFromInternalStorageIntoRecyclerView()
+    }
+
+    private fun updateOrRequestPermissions() {
+        val hasReadPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val hasWritePermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        ) == PackageManager.PERMISSION_GRANTED
+
+        // Write external permission not needed after sdk level 29.
+        val minSdk29 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+
+        readPermissionGranted = hasReadPermission
+        writePermissionGranted = hasWritePermission || minSdk29
+
+        val permissionsToRequest = mutableListOf<String>()
+        if(!writePermissionGranted) {
+            permissionsToRequest.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if(!readPermissionGranted) {
+            permissionsToRequest.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        // Note: In production app, if user denies or permanently denies permissions. Send
+        // dialogue saying app cannot work without these permissions.
+        if(permissionsToRequest.isNotEmpty()) {
+            permissionLauncher.launch(permissionsToRequest.toTypedArray())
+        }
+    }
+
+    private fun savePhotoToExternalStorage(displayName: String, bmp: Bitmap): Boolean {
+        // Media store - Database for all types of media files and corresponding metadata
+        // Get collection for images and save into media store
+        // Need to get URI which depend on the version. URI will be like the address of the file
+        val imageCollection = sdk29AndUp {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } ?: MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        // Meta data for the file
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "$displayName.jpg")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.HEIGHT, bmp.height)
+            put(MediaStore.Images.Media.DISPLAY_NAME, bmp.width)
+        }
+
+        return try {
+            /*
+            To actually store into media store first we need a content resolver, which will place
+            the metadata along with the uri provided depending on sdk version. Once that is done,
+            content resovler will give us another uri leading to the actually media store database.
+            Here we can open up an output stream where we store the file just like we did with
+            local storage.
+             */
+            contentResolver.insert(imageCollection, contentValues)?.also { uri ->
+                contentResolver.openOutputStream(uri).use { outputStream ->
+                    if(!bmp.compress(Bitmap.CompressFormat.JPEG, 95, outputStream)) {
+                        throw IOException("Couldn't save bitmap")
+                    }
+                }
+            } ?: throw IOException("Coudln't create MediaStore Entry")
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
     }
 
     private fun setupInternalStorageRecyclerView() = binding.rvPrivatePhotos.apply {
